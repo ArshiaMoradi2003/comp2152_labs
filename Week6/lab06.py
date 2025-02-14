058acf327898b0bfcfde39a9bb6e8590b32b4bb9
ArshiaMoradi2003 <arshiamoradi1203@gmail.com>
2025-02-14 09:14:15 -0500

    add functions_lab06.adjust_combat_strength(combat_strength, m_combat_strength) - Question 5b

--- Week6/lab06.py
+++ Week6/lab06.py
@@ -89,7 +89,7 @@ if not input_invalid:
     print("    |    The hero\'s weapon is " + str(weapons[weapon_roll - 1]))
 
     # Lab 06 - Question 5b
-
+    functions_lab06.adjust_combat_strength(combat_strength, m_combat_strength)
     # Weapon Roll Analysis
     print("    ------------------------------------------------------------------")
     print("    |", end="    ")
