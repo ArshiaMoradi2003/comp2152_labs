677434698581171a47391ee6613467e94f9b518c
ArshiaMoradi2003 <arshiamoradi1203@gmail.com>
2025-03-14 08:55:19 -0400

    fix query_2 del_row fixed

--- Week10/main.py
+++ Week10/main.py
@@ -20,7 +20,7 @@ try:
             try:
                 del_row = int(input("Enter the row ID threshold for deletion:"))
                 query_2 = "DELETE FROM demo WHERE id < ?"
-                cursor.execute(query_2, (del_row))
+                cursor.execute(query_2, (del_row,))
                 num_rows = cursor.rowcount
                 print(f"{num_rows} rows affected.")
                 db_conn.commit()
